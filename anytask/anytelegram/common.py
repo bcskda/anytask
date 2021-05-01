8971a86a65953a25ae10274c8f114077da3a519a
Dmitrii Kuptsov <demulkup@ya.ru>
2021-05-01 21:21:58 +0300

    CST-85 use python-telegram-bot to manage webhook

--- anytask/anytelegram/common.py
+++ anytask/anytelegram/common.py
@@ -1,3 +1,7 @@
+from django.conf import settings
+from telegram import Bot, TelegramError
+from telegram.ext import Dispatcher
+
 from mail.base import BaseRenderer, BaseSender
 
 import logging
@@ -6,8 +10,47 @@ import logging
 logger = logging.getLogger('django.request')
 
 
-def bot_endpoint(token):
-    return 'https://api.telegram.org/bot{}'.format(token)
+class AnyTelegram:
+    """Not thread-safe: must be called from single thread"""
+
+    _token = settings.TELEGRAM_TOKEN
+    _webhook_url = settings.TELEGRAM_WEBHOOK_URL
+
+    def __init__(self):
+        self._bot = None
+        self._dispatcher = None
+
+    def get_bot(self):
+        if self._bot is None:
+            self._bot = Bot(self._token)
+        return self._bot
+
+    def get_dispatcher(self):
+        if self._dispatcher is None:
+            self._dispatcher = Dispatcher(self.get_bot(), update_queue=None, workers=0)
+        return self._dispatcher
+
+    def set_webhook(self, **kwargs):
+        try:
+            return self.get_bot().set_webhook(url=self.get_webhook_url(), **kwargs)
+        except TelegramError as e:
+            logger.exception('set_webhook', exc_info=e)
+            return False
+
+    def delete_webhook(self):
+        try:
+            return self.get_bot().delete_webhook()
+        except TelegramError as e:
+            logger.exception('delete_webhook', exc_info=e)
+            return False
+
+    @classmethod
+    def get_bot_endpoint(cls):
+        return 'https://api.telegram.org/bot{}'.format(cls._token)
+
+    @classmethod
+    def get_webhook_url(cls):
+        return cls._webhook_url
 
 
 class TelegramRenderer(BaseRenderer):
