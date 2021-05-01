94bf031921e119a05cd789fe5f1337e4ad07c5f1
Dmitrii Kuptsov <demulkup@ya.ru>
2021-05-01 21:21:58 +0300

    CST-85 use python-telegram-bot to manage webhook

--- anytask/anytelegram/management/commands/tg_attach_webhook.py
+++ anytask/anytelegram/management/commands/tg_attach_webhook.py
@@ -1,20 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from django.core.management.base import BaseCommand
-from django.conf import settings
-import requests
 
-from anytask.anytelegram.common import bot_endpoint
+from anytask.anytelegram.common import AnyTelegram
 
 
 class Command(BaseCommand):
     """Calls setWebhook method"""
 
-    def __init__(self, stdout=None, stderr=None, no_color=False):
-        super(Command, self).__init__()
-        token = settings.TELEGRAM_TOKEN
-        self.bot_endpoint = bot_endpoint(token)
-        self.webhook_url = settings.TELEGRAM_WEBHOOK_URL
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
+        self.api = AnyTelegram()
+        self.bot_endpoint = AnyTelegram.get_webhook_url()
+        self.webhook_url = AnyTelegram.get_webhook_url()
 
     def add_arguments(self, parser):
         """Call super when overriding"""
@@ -27,19 +25,10 @@ class Command(BaseCommand):
         )
 
     def handle(self, **options):
-        webhook_info = requests.get(self.bot_endpoint + '/webhookInfo')
-        self.stdout.write('webhookInfo: {} {}'.format(webhook_info.status_code, webhook_info.text))
-
-        set_result = requests.post(
-            self.bot_endpoint + '/setWebhook',
-            params={
-                'url': self.webhook_url,
-                'drop_pending_updates': options['drop_pending_updates']
-            }
-        )
-        self.stdout.write('setWebhook: {} {}'.format(set_result.status_code, set_result.text))
-        if set_result.status_code != 200:
-            self.stdout.write('setWebhook failed, exiting')
-            return
-
-        self.stdout.write('Webhook set, no errors')
+        webhook_info = self.api.get_bot().get_webhook_info()
+        self.stdout.write('webhookInfo: {}'.format(webhook_info))
+        set_success = self.api.set_webhook(drop_pending_updates=options['drop_pending_updates'])
+        if not set_success:
+            self.stdout.write('Setting webhook failed')
+        else:
+            self.stdout.write('Webhook set, no errors')
--- anytask/anytelegram/management/commands/tg_detach_webhook.py
+++ anytask/anytelegram/management/commands/tg_detach_webhook.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from django.core.management.base import BaseCommand
-from django.conf import settings
-import requests
 
-from anytask.anytelegram.common import bot_endpoint
+from anytask.anytelegram.common import AnyTelegram
 
 
 class Command(BaseCommand):
@@ -12,38 +10,13 @@ class Command(BaseCommand):
 
     def __init__(self, stdout=None, stderr=None, no_color=False):
         super(Command, self).__init__()
-        token = settings.TELEGRAM_TOKEN
-        self.bot_endpoint = bot_endpoint(token)
-
-    def add_arguments(self, parser):
-        """Call super when overriding"""
-        parser.add_argument(
-            '--drop-pending-updates',
-            dest='drop_pending_updates',
-            action='store_true',
-            default=False,
-            help='Ask API to drop pending updates'
-        )
+        self.api = AnyTelegram()
 
     def handle(self, **options):
-        webhook_info = requests.get(self.bot_endpoint + '/webhookInfo')
-        self.stdout.write('webhookInfo: {} {}'.format(webhook_info.status_code, webhook_info.text))
-        if webhook_info.status_code == 200:
-            pass
-        elif webhook_info.status_code == 404:
-            self.stdout.write('No webhook was attached, nothing changed')
-            return
+        webhook_info = self.api.get_bot().get_webhook_info()
+        self.stdout.write('webhookInfo: {}'.format(webhook_info))
+        delete_success = self.api.delete_webhook()
+        if not delete_success:
+            self.stdout.write('Deleting webhook failed, exiting')
         else:
-            self.stdout.write('webHookInfo failed, exiting')
-            return
-
-        delete_result = requests.post(
-            self.bot_endpoint + '/deleteWebhook',
-            params={'drop_pending_updates': options['drop_pending_updates']}
-        )
-        self.stdout.write('deleteWebhook: {} {}'.format(delete_result.status_code, delete_result.text))
-        if delete_result.status_code != 200:
-            self.stdout.write('deleteWebhook failed, exiting')
-            return
-
-        self.stdout.write('Webhook deleted, no errors')
+            self.stdout.write('Webhook deleted, no errors')
