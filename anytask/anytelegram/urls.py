66cfd0d4b6c76ab04603ed5eecd224208521652f
Dmitrii Kuptsov <demulkup@ya.ru>
2021-05-01 15:42:37 +0300

    CST-84 Wire webhook view

--- anytask/anytelegram/urls.py
+++ anytask/anytelegram/urls.py
@@ -2,6 +2,6 @@ import anytelegram.views
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^webhook/(?P<token>[0-9a-z\-]+)$', anytelegram.views.webhook,
+    url(r'^webhook/(?P<token>[0-9a-zA-Z_:\-\+]+)$', anytelegram.views.webhook,
         name='anytelegram.views.webhook')
 ]
